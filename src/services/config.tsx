a151ff63496669605e44374720aa9a3be18049f2
Roger Toledo <roger.toledo@olxbr.com>
2026-07-12 20:44:35 -0300

    refactor: start to use env

--- src/services/config.tsx
+++ src/services/config.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:8180',
+    baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://127.0.0.1:8180',
     timeout: 15000,
     headers: {
       'Content-Type': 'application/json',
